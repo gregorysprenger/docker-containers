414c968559ea478ef857092b211aa64d17e7a840
Gregory Sprenger <42686628+gregorysprenger@users.noreply.github.com>
2023-06-16 14:41:43 -0400

    add kraken2 v2.1.3

---
+++ kraken2/2.1.3/tests/test_versions.py
@@ -0,0 +1,16 @@
+import unittest
+import subprocess
+
+
+class TestVersion(unittest.TestCase):
+    def test_kraken2(self):
+        command = "kraken2 --version | head -n 1 | awk '{print $3}'"
+        process = subprocess.Popen(
+            command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
+        out, err = process.communicate()
+        self.assertEqual(out, b"2.1.3\n")
+
+
+if __name__ == "__main__":
+    unittest.main()
