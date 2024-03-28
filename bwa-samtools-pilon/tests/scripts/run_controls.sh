3e0ca862dd6bb9720ff4bdce5209924c0eecda0c
Gregory Sprenger <42686628+gregorysprenger@users.noreply.github.com>
2024-03-28 14:13:04 -0500

    Simplify wget commands; decompress reference file

--- bwa-samtools-pilon/tests/scripts/run_controls.sh
+++ bwa-samtools-pilon/tests/scripts/run_controls.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
 # Download test data
-wget https://github.com/nf-core/test-datasets/raw/viralrecon/illumina/sispa/SRR11140744_R1.fastq.gz
-wget https://github.com/nf-core/test-datasets/raw/viralrecon/illumina/sispa/SRR11140744_R2.fastq.gz
-wget https://github.com/nf-core/test-datasets/raw/viralrecon/genome/MN908947.3/GCA_009858895.3_ASM985889v3_genomic.200409.fna.gz -O reference.fna.gz
+wget \
+  https://github.com/nf-core/test-datasets/raw/viralrecon/illumina/sispa/SRR11140744_R1.fastq.gz \
+  https://github.com/nf-core/test-datasets/raw/viralrecon/illumina/sispa/SRR11140744_R2.fastq.gz
+
+wget \
+  https://github.com/nf-core/test-datasets/raw/viralrecon/genome/MN908947.3/GCA_009858895.3_ASM985889v3_genomic.200409.fna.gz \
+  -O reference.fna.gz
 
 # Create bwa index
-bwa index reference.fna.gz
+gunzip reference.fna.gz
+bwa index reference.fna
 
 # Align PE reads
 bwa mem \
   -v 2 \
-  reference.fna.gz \
+  reference.fna \
   SRR11140744_R1.fastq.gz \
   SRR11140744_R2.fastq.gz \
   > test.sam
@@ -20,7 +25,7 @@ sha256sum test.sam > test.sam.checksum
 
 # Sort and index reads
 samtools sort \
-  --reference reference.fna.gz \
+  --reference reference.fna \
   -l 9 \
   -o paired.bam \
   test.sam
@@ -31,7 +36,7 @@ sha256sum paired.bam > paired.bam.checksum
 
 # Get representative genome
 pilon \
-  --genome reference.fna.gz \
+  --genome reference.fna \
   --frags paired.bam \
   --output "pilon"
 
