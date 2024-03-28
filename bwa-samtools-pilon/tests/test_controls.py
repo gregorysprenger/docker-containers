9014056e25a3721483fa52dbffa66b0784ca6ed2
Gregory Sprenger <42686628+gregorysprenger@users.noreply.github.com>
2024-03-28 14:13:27 -0500

    Update checksums in assert statements

--- bwa-samtools-pilon/tests/test_controls.py
+++ bwa-samtools-pilon/tests/test_controls.py
@@ -10,23 +10,23 @@ class TestControls(unittest.TestCase):
         subprocess.run(command, shell=True, stdout=PIPE)
 
     def test_bwa(self):
-        with open("data/test.sam.checksum") as f:
+        with open("test.sam.checksum") as f:
             bwa_checksum = f.readlines()[0].split(" ")[0]
         self.assertEqual(
             bwa_checksum,
-            "821f4f4bd08f50035f7f99c3180d1ec8a7ff028d1e743530cc7bd775ca3757b5",
+            "cdfb47712f8a4e79da7969518fcde1ed7469d9d495072f7286cc9271b48ab769",
         )
 
     def test_samtools(self):
-        with open("data/paired.bam.checksum") as f:
+        with open("paired.bam.checksum") as f:
             samtools_checksum = f.readlines()[0].split(" ")[0]
         self.assertEqual(
             samtools_checksum,
-            "d3d9c8bc14c3c81b26d5cc0eaec5c8f687c696a269afeb5edacb1982fd2f00f1",
+            "20e892e75472b5f2a8a2bd8e5b03c8227300bbf356601012b72f839ed13fe86e",
         )
 
     def test_pilon(self):
-        with open("data/pilon.fasta.checksum") as f:
+        with open("pilon.fasta.checksum") as f:
             pilon_checksum = f.readlines()[0].split(" ")[0]
         self.assertEqual(
             pilon_checksum,
